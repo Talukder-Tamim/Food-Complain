10aef16867fd9fec5f706f14aee3f5decc304cca
Talukder-Tamim <talukder.tamim@gmail.com>
2020-07-08 10:17:40 +0600

    FoodComplain

---
+++ auth/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def registerView(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account successfully created for " + user)
+            return redirect('login')
+
+    else:
+        form = UserForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginView(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'accounts/login.html')
